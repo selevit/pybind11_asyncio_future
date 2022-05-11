3963c2314a9b9cfb326aed2571c29cd9a7a8dc32
Sergey Levitin <s.levitin@sparkland-traading.com>
2022-05-11 14:04:08 +0000

    Mac support

--- eventfd.cpp
+++ eventfd.cpp
@@ -1,8 +1,6 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
-#include <sys/epoll.h>
-#include <sys/eventfd.h>
 #include <pybind11/functional.h>
 #include <pybind11/stl.h>
 
@@ -10,6 +8,49 @@
 using namespace std;
 namespace py = pybind11;
 
+#ifdef __linux__
+    #include <sys/eventfd.h>
+#else
+    #include <unistd.h>
+#endif
+
+class EventFd {
+public:
+    EventFd() {
+        #ifdef __linux__
+            read_fd = eventfd(0, EFD_NONBLOCK);
+            write_fd = read_fd;
+        #else
+            int buf[2];
+            pipe(buf);
+            read_fd = buf[0];
+            write_fd = buf[1];
+        #endif
+    }
+
+    void clear() {
+        uint8_t buf[8];
+        read(read_fd, &buf, sizeof(uint64_t));
+    }
+
+    void notify() {
+        uint64_t new_event_flag = 1;
+        write(write_fd, &new_event_flag, sizeof(uint64_t));
+    }
+
+    int fd() {
+        return read_fd;
+    }
+
+    ~EventFd() {
+        close(read_fd);
+        close(write_fd);
+    }
+
+private:
+    int read_fd;
+    int write_fd;
+};
 
 using PyTickerCallback = std::function<void(int)>;
 
@@ -18,11 +59,9 @@ class Ticker {
         Ticker(PyTickerCallback const& ticker_cb) {
             ticker_value = 1;
             ticker_callback = ticker_cb;
-            event_fd = eventfd(0, EFD_NONBLOCK);
             py::object loop = py::module_::import("asyncio").attr("get_running_loop")();
-            loop.attr("add_reader")(event_fd, py::cpp_function([this] {
-                uint8_t buf[8];
-                read(event_fd, &buf, sizeof(uint64_t));
+            loop.attr("add_reader")(event.fd(), py::cpp_function([this] {
+                event.clear();
                 this->ticker_callback(ticker_value);
             }));
             subscribe();
@@ -31,25 +70,23 @@ class Ticker {
         void subscribe() {
             producing_thread = std::thread([this] {
                 cout << "[cpp] Ticker subscribtion started" << endl;
-                uint64_t new_event_flag = 1;
                 while (true) {
-                    write(event_fd, &new_event_flag, sizeof(uint64_t));
                     ticker_value += 1;
+                    event.notify();
                     std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
             });
         }
 
-        int event_fd;
         int ticker_value;
     private:
         std::thread producing_thread;
         PyTickerCallback ticker_callback;
+        EventFd event;
 };
 
 PYBIND11_MODULE(myeventfd, m) {
     py::class_<Ticker>(m, "Ticker")
         .def(py::init<PyTickerCallback const&>(), py::arg("ticker_callback"))
-        .def_readonly("event_fd", &Ticker::event_fd)
         .def_readonly("ticker_value", &Ticker::ticker_value);
 }
